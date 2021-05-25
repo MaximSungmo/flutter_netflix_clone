080cab8377fa6cc93e28cfa57cb8e36f0bd74755
MaximSungmo <ksm5318@naver.com>
2021-05-26 00:43:57 +0900

    feat: Add Movie dummy data

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/model/model_movie.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/;로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -31,15 +41,24 @@ class TopBar extends StatelessWidget {
           ),
           Container(
             padding: EdgeInsets.only(right: 1),
-            child: Text('TV 프로그램', style: TextStyle(fontSize: 14),),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
           ),
           Container(
             padding: EdgeInsets.only(right: 1),
-            child: Text('영화', style: TextStyle(fontSize: 14),),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
           ),
           Container(
             padding: EdgeInsets.only(right: 1),
-            child: Text('내가 찜한 콘텐츠', style: TextStyle(fontSize: 14),),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
           )
         ],
       ),
